7096c215cda71513e823426d28726c3426c23771
Farazkhan <farazkhantaraka@gmail.com>
2023-11-29 15:23:53 +0500

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById('p1').setAttribute('class','Deins-Height');
+
+/////////////////////////////////////////////////////////////////////
+const value1=document.getElementById('p1').innerText="this is farazkhan"
+console.log(value1)
+const value2=document.getElementById('p1').innerHTML='<span>Hello World</span>'
+
+console.log(value2)
+
+// value.style.backgroundColor='green';
+// value.style.padding='20px';
+// value.style.margin='30px';
+// value.style.borderRadius='10px';
+// value.style.borderRight='10px'
+// value.style.borderColor='black';
+// value.style.backgroundColor='grey'
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
